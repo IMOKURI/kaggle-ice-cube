328e2009b6b11ebbb87ac3905a14fb055e5b8ef5
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2023-02-24 11:02:33 +0000

    feat: Add 180d TTA.
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- 09-inference.py
+++ 09-inference.py
@@ -4,7 +4,7 @@ import hydra
 import pandas as pd
 
 import src.utils as utils
-from src.ice_cube.data_loader import make_test_dataloader, make_train_dataloader
+from src.ice_cube.data_loader import make_test_dataloader, make_test_dataloader_plus_minus, make_train_dataloader
 from src.ice_cube.model import load_pretrained_model
 from src.ice_cube.scoring import angular_dist_score
 from src.ice_cube.submission import to_submission_df
@@ -21,12 +21,9 @@ def main(c):
     utils.basic_environment_info()
     utils.fix_seed(utils.choice_seed(c))
 
-    if c.settings.is_training:
-        _, dataloader = make_train_dataloader(c)
-    else:
-        dataloader = make_test_dataloader(c)
-
+    dataloader = make_test_dataloader(c)
     model = load_pretrained_model(c, dataloader)
+
     results = model.predict_as_dataframe(
         gpus=[0],
         dataloader=dataloader,
@@ -35,7 +32,24 @@ def main(c):
     )
 
     submission_df = to_submission_df(results)
-    submission_df.to_csv("submission.csv")
+    # submission_df.to_csv("submission.csv")
+
+    if c.inference_params.tta:
+        log.info(f"Enable TTA.")
+        dataloader = make_test_dataloader_plus_minus(c)
+        results_tta = model.predict_as_dataframe(
+            gpus=[0],
+            dataloader=dataloader,
+            prediction_columns=model.prediction_columns,
+            additional_attributes=[c.settings.index_name],
+        )
+
+        results_tta["direction_x"] = -results_tta["direction_x"]
+        results_tta["direction_y"] = -results_tta["direction_y"]
+        results_tta["direction_z"] = -results_tta["direction_z"]
+
+        submission_tta_df = to_submission_df(results_tta)
+        # submission_tta_df.to_csv("submission_tta.csv")
 
     if c.settings.is_training:
         valid_data = dataloader.dataset.query_table("meta_table", ["event_id", "azimuth", "zenith"])
@@ -50,6 +64,17 @@ def main(c):
         log.info(f"score: {score}")
         # valid_df.to_csv("valid.csv")
 
+        if c.inference_params.tta:
+            score = angular_dist_score(
+                valid_df["azimuth"], valid_df["zenith"], submission_tta_df["azimuth"], submission_tta_df["zenith"]
+            )
+            log.info(f"TTA score: {score}")
+
+    if c.inference_params.tta:
+        results = (results + results_tta) / 2
+        submission_df = to_submission_df(results)
+
+    submission_df.to_csv("submission.csv")
     log.info("Done.")
 
 
--- src/ice_cube/data_loader.py
+++ src/ice_cube/data_loader.py
@@ -1,8 +1,13 @@
 import os
+from typing import List
 
+import torch
 from graphnet.data.constants import FEATURES, TRUTH
+from graphnet.data.sqlite import SQLiteDataset
 from graphnet.training.labels import Direction
 from graphnet.training.utils import make_dataloader, make_train_validation_dataloader
+from torch.utils.data import DataLoader
+from torch_geometric.data import Batch, Data
 
 
 def make_train_dataloader(c):
@@ -16,7 +21,7 @@ def make_train_dataloader(c):
         truth=TRUTH.KAGGLE,
         labels={"direction": Direction()},
         batch_size=c.training_params.batch_size,
-        # num_workers=c.training_params.num_workers,
+        num_workers=c.training_params.num_workers,
         index_column=c.settings.index_name,
         truth_table=c.data.ice_cube.meta_table,
         seed=c.global_params.seed,
@@ -27,7 +32,10 @@ def make_train_dataloader(c):
 
 
 def make_test_dataloader(c):
-    database_path = os.path.join(c.data.dir.dataset, "test_db.db")
+    if c.settings.is_training:
+        database_path = os.path.join(c.data.dir.dataset, f"train_{c.data.ice_cube.train_batch}_db.db")
+    else:
+        database_path = os.path.join(c.data.dir.dataset, "test_db.db")
 
     dataloader = make_dataloader(
         db=database_path,
@@ -35,12 +43,86 @@ def make_test_dataloader(c):
         pulsemaps=c.data.ice_cube.pulse_table,
         features=FEATURES.KAGGLE,
         truth=TRUTH.KAGGLE,
-        labels={"direction": Direction()},
+        # labels={"direction": Direction()},
         batch_size=c.training_params.batch_size,
         shuffle=False,
-        # num_workers=c.training_params.num_workers,
+        num_workers=c.training_params.num_workers,
         index_column=c.settings.index_name,
         truth_table=c.data.ice_cube.meta_table,
     )
 
     return dataloader
+
+
+def make_test_dataloader_plus_minus(c):
+    """
+    https://graphnet-team.github.io/graphnet/api/graphnet.training.utils.html#graphnet.training.utils.make_dataloader
+    """
+    if c.settings.is_training:
+        database_path = os.path.join(c.data.dir.dataset, f"train_{c.data.ice_cube.train_batch}_db.db")
+    else:
+        database_path = os.path.join(c.data.dir.dataset, "test_db.db")
+
+    db = database_path
+    selection = None  # Entire database
+    pulsemaps = c.data.ice_cube.pulse_table
+    features = FEATURES.KAGGLE
+    truth = TRUTH.KAGGLE
+    labels = None  # labels={"direction": Direction()}
+    batch_size = c.training_params.batch_size
+    shuffle = False
+    num_workers = c.training_params.num_workers
+    index_column = c.settings.index_name
+    truth_table = c.data.ice_cube.meta_table
+
+    if isinstance(pulsemaps, str):
+        pulsemaps = [pulsemaps]
+
+    dataset = SQLiteDataset(
+        path=db,
+        pulsemaps=pulsemaps,
+        features=features,
+        truth=truth,
+        selection=selection,
+        node_truth=None,
+        truth_table=truth_table,
+        node_truth_table=None,
+        string_selection=None,
+        loss_weight_table=None,
+        loss_weight_column=None,
+        index_column=index_column,
+    )
+
+    # adds custom labels to dataset
+    if isinstance(labels, dict):
+        for label in labels.keys():
+            dataset.add_label(key=label, fn=labels[label])
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers,
+        collate_fn=collate_fn_plus_minus,
+        persistent_workers=True,
+        prefetch_factor=2,
+    )
+
+    return dataloader
+
+
+def collate_fn_plus_minus(graphs: List[Data]) -> Batch:
+    """Remove graphs with less than two DOM hits.
+
+    Should not occur in "production.
+    """
+    batch = []
+    for data in graphs:
+        data.x = torch.mul(data.x, torch.FloatTensor([-1, -1, -1, 1, 1, 1]))
+        data.y = -data.y
+        data.z = -data.z
+
+        if data.n_pulses > 1:
+            batch.append(data)
+
+    return Batch.from_data_list(batch)
