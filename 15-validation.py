cd64be4206edfd03c7244997bfbeb4013873e1ee
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2023-03-27 13:33:59 +0000

    refactor: remove linear regression.
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- 15-validation.py
+++ 15-validation.py
@@ -22,10 +22,6 @@ def main(c):
         log.info("Validation stage 2.")
         results = pd.read_csv("results_stage2.csv").set_index("event_id")
         submission_df = pd.read_csv("submission_stage2.csv").set_index("event_id")
-    elif c.training_params.linear_regression:
-        log.info("Validation linear regression.")
-        results = pd.read_csv("results_linear_regression.csv").set_index("event_id")
-        submission_df = pd.read_csv("submission_linear_regression.csv").set_index("event_id")
     else:
         log.info("Validation stage 1.")
         results = pd.read_csv("results.csv").set_index("event_id")
@@ -38,10 +34,6 @@ def main(c):
             results["azimuth_y"], results["zenith_y"], submission_df["azimuth"], submission_df["zenith"]
         )
         log.info(f"Base score: {score}")
-
-    if c.training_params.linear_regression:
-        log.info("Done.")
-        return
 
     results_low_sigma = results[results["sigma"] <= 0.5]
     results_high_sigma = results[results["sigma"] > 0.5]
--- src/ice_cube/data_loader.py
+++ src/ice_cube/data_loader.py
@@ -1,11 +1,12 @@
-from typing import Callable, List, Optional
+from typing import Callable, Dict, List, Optional, Union
 
 import numpy as np
 import pandas as pd
 import torch
 from graphnet.data.constants import FEATURES, TRUTH
+from graphnet.data.sqlite import SQLiteDataset
 from graphnet.training.labels import Direction
-from graphnet.training.utils import make_train_validation_dataloader
+from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Dataset
 from torch_geometric.data import Batch, Data
 
@@ -82,23 +83,148 @@ class IceCubeBatchDataset(Dataset):
         return data
 
 
-def make_sqlite_dataloader(c, database_path, selection=None):
-    train_loader, valid_loader = make_train_validation_dataloader(
-        db=database_path,
-        selection=selection,  # Entire database
-        pulsemaps=c.data.ice_cube.pulse_table,
-        features=FEATURES.KAGGLE,
-        truth=TRUTH.KAGGLE,
-        labels={"direction": Direction()},
-        batch_size=c.training_params.batch_size,
-        num_workers=c.training_params.num_workers,
-        index_column=c.settings.index_name,
-        truth_table=c.data.ice_cube.meta_table,
-        seed=c.global_params.seed,
-        test_size=0.2,
+def make_dataloader(
+    db: str,
+    pulsemaps: Union[str, List[str]],
+    features: List[str],
+    truth: List[str],
+    *,
+    batch_size: int,
+    shuffle: bool,
+    selection: Optional[List[int]] = None,
+    num_workers: int = 10,
+    persistent_workers: bool = True,
+    node_truth: List[str] = None,
+    truth_table: str = "truth",
+    node_truth_table: Optional[str] = None,
+    string_selection: List[int] = None,
+    loss_weight_table: Optional[str] = None,
+    loss_weight_column: Optional[str] = None,
+    index_column: str = "event_no",
+    labels: Optional[Dict[str, Callable]] = None,
+    collate_fn: Callable,
+) -> DataLoader:
+    """Construct `DataLoader` instance."""
+    # Check(s)
+    if isinstance(pulsemaps, str):
+        pulsemaps = [pulsemaps]
+
+    dataset = SQLiteDataset(
+        path=db,
+        pulsemaps=pulsemaps,
+        features=features,
+        truth=truth,
+        selection=selection,
+        node_truth=node_truth,
+        truth_table=truth_table,
+        node_truth_table=node_truth_table,
+        string_selection=string_selection,
+        loss_weight_table=loss_weight_table,
+        loss_weight_column=loss_weight_column,
+        index_column=index_column,
     )
 
-    return train_loader, valid_loader
+    # adds custom labels to dataset
+    if isinstance(labels, dict):
+        for label in labels.keys():
+            dataset.add_label(key=label, fn=labels[label])
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers,
+        collate_fn=collate_fn,
+        persistent_workers=persistent_workers,
+        prefetch_factor=2,
+    )
+
+    return dataloader
+
+
+def make_sqlite_dataloader(c, database_path, selection=None):
+    db = database_path
+    selection = selection  # Entire database
+    pulsemaps = c.data.ice_cube.pulse_table
+    features = FEATURES.KAGGLE
+    truth = TRUTH.KAGGLE
+    labels = {"direction": Direction()}
+    batch_size = c.training_params.batch_size
+    num_workers = c.training_params.num_workers
+    index_column = c.settings.index_name
+    truth_table = c.data.ice_cube.meta_table
+    seed = c.global_params.seed
+    test_size = 0.2
+
+    # Reproducibility
+    rng = np.random.RandomState(seed=seed)
+
+    # Checks(s)
+    if isinstance(pulsemaps, str):
+        pulsemaps = [pulsemaps]
+
+    if selection is None:
+        # If no selection is provided, use all events in dataset.
+        dataset: Dataset
+        if db.endswith(".db"):
+            dataset = SQLiteDataset(
+                db,
+                pulsemaps,
+                features,
+                truth,
+                truth_table=truth_table,
+                index_column=index_column,
+            )
+        else:
+            raise RuntimeError(f"File {db} with format {db.split('.'[-1])} not supported.")
+        selection = dataset._get_all_indices()
+
+    # Perform train/validation split
+    if isinstance(db, list):
+        df_for_shuffle = pd.DataFrame({"event_no": selection, "db": None})
+        shuffled_df = df_for_shuffle.sample(frac=1, replace=False, random_state=rng)
+        training_df, validation_df = train_test_split(shuffled_df, test_size=test_size, random_state=rng)
+        training_selection = training_df.values.tolist()
+        validation_selection = validation_df.values.tolist()
+    else:
+        training_selection, validation_selection = train_test_split(selection, test_size=test_size, random_state=rng)
+
+    # Create DataLoaders
+    common_kwargs = dict(
+        db=db,
+        pulsemaps=pulsemaps,
+        features=features,
+        truth=truth,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        persistent_workers=None,
+        node_truth=None,
+        truth_table=truth_table,
+        node_truth_table=None,
+        string_selection=None,
+        loss_weight_column=None,
+        loss_weight_table=None,
+        index_column=index_column,
+        labels=labels,
+        collate_fn=collate_fn_training,
+    )
+
+    training_dataloader = make_dataloader(
+        shuffle=True,
+        selection=training_selection,
+        **common_kwargs,  # type: ignore[arg-type]
+    )
+
+    validation_dataloader = make_dataloader(
+        shuffle=False,
+        selection=validation_selection,
+        **common_kwargs,  # type: ignore[arg-type]
+    )
+
+    return (
+        training_dataloader,
+        validation_dataloader,
+    )
 
 
 def make_dataloader_batch(
